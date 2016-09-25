0c88b607eb8075ca9a5d121418bbf4bb1650f085
157T <>
2016-09-25 19:24:27 -0300

    Implementaçao da classe SaldoInsuficienteException.

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
@@ -5,4 +5,10 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class SaldoInsuficienteException extends Exception {
+    private static final String SaldoInsuficiente="Saldo insuficiente";
+    
+    public SaldoInsuficienteException(){
+        super(SaldoInsuficiente);
+    }
+    
 }
