d9c512e5ab5bcd4ea73332e5868a4f4bdd2a5ae3
Usuario=Lucas <>
2016-09-25 19:57:51 -0300

    Mudanca no metodo inserir da classe TicketMachine

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -21,9 +21,10 @@ public class TicketMachine {
 
     public void inserir(int quantia) throws PapelMoedaInvalidaException {
         boolean achou = false;
-        for (int i = 0; i < papelMoeda.length && !achou; i++) {
-            if (papelMoeda[1] == quantia) {
+        for (int i = 0; i < papelMoeda.length -1; i++) {
+            if (papelMoeda[i] == quantia) {
                 achou = true;
+                break;
             }
         }
         if (!achou) {
