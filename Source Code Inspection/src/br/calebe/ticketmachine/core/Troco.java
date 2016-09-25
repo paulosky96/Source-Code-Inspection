427e613186d919e931c55f42ed64f95770afa25b
157T <157@192.168.0.24>
2016-09-25 20:38:38 -0300

    Revert "Mudançao no construtor da classe Troco"
    This reverts commit 755933d07c594992fed8817cfdc7f737f534c29f.

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -22,19 +22,21 @@ class Troco {
      }
 
     public Iterator<PapelMoeda> getIterator() {
-        return new TrocoIterator();
+        return new TrocoIterator(this);
     }
 
     class TrocoIterator implements Iterator<PapelMoeda> {
 
-        
+        protected Troco troco;
 
-       
+        public TrocoIterator(Troco troco) {
+            this.troco = troco;
+        }
 
         @Override
         public boolean hasNext() {
             for (int i = 6; i >= 0; i++) {
-                if (papeisMoeda[i] != null) {
+                if (troco.papeisMoeda[i] != null) {
                     return true;
                 }
             }
@@ -45,9 +47,9 @@ class Troco {
         public PapelMoeda next() {
             PapelMoeda ret = null;
             for (int i = 6; i >= 0 && ret != null; i++) {
-                if (papeisMoeda[i] != null) {
-                    ret = papeisMoeda[i];
-                    papeisMoeda[i] = null;
+                if (troco.papeisMoeda[i] != null) {
+                    ret = troco.papeisMoeda[i];
+                    troco.papeisMoeda[i] = null;
                 }
             }
             return ret;
